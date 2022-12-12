da7e6ea1f4120f9cd0b79d5a083cdf90a0c4b009
Julia Schmid <schmidja@hitssv566.villa-bosch.de>
2022-12-12 15:04:38 +0000

    fix data collection script

--- final_data_collection.py
+++ final_data_collection.py
@@ -29,16 +29,18 @@ if __name__ == "__main__":
 
         rax_df = pd.read_parquet(raxmlng_parquet)
         rax_df.is_best = rax_df.is_best.astype(bool)
+
         rax_df = rax_df.loc[rax_df.is_best]
 
         assert rax_df.shape[0] >= 1, f"The data for MSA {msa_dir} does not seem to contain a best_tree, please check the RAxML-NG logs..."
 
+        rax_df = rax_df.drop(["id", "uuid", "dataset_id", "dataset_uuid"], axis=1)
         rax_df = rax_df.head(1)
 
         df = pd.concat([df, rax_df], axis=1)
         dfs.append(df)
 
-    final_df = pd.concat(dfs)
+    final_df = pd.concat(dfs, ignore_index=True)
     final_df.to_parquet(directory / "all_data.parquet")
 
 
